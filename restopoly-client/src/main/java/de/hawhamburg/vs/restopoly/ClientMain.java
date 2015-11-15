d6ba2fc8fe9f4104d12302f3ddb7ce00e2e14b07
Tim Hagemann <tim@infinityblade.de>
2015-11-15 14:20:09 +0100

    Some more work on client.

--- restopoly-client/src/main/java/de/hawhamburg/vs/restopoly/ClientMain.java
+++ restopoly-client/src/main/java/de/hawhamburg/vs/restopoly/ClientMain.java
@@ -1,11 +1,13 @@
 package de.hawhamburg.vs.restopoly;
 
 import de.hawhamburg.vs.restopoly.api.GameService;
-import de.hawhamburg.vs.restopoly.responses.GameCreateResponse;
 import retrofit.GsonConverterFactory;
+import retrofit.Response;
 import retrofit.Retrofit;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ClientMain {
     public static void main(String[] args) throws IOException {
@@ -13,8 +15,26 @@ public class ClientMain {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         GameService service = retrofit.create(GameService.class);
-        GameCreateResponse response = service.createGame().execute().body();
-        System.out.println(response.gameid);
-        System.out.println(service.getGames().execute().body().size());
+
+        System.out.print("[C]reate or [j]oin game? : ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String answer = reader.readLine();
+        int gameid;
+        if(answer.equals("j")){
+            System.out.print("Enter game id: ");
+            gameid = Integer.parseInt(reader.readLine());
+        } else {
+            gameid = service.createGame().execute().body().gameid;
+            System.out.println("Created game " + gameid);
+        }
+
+        System.out.print("Enter name: ");
+        String name = reader.readLine();
+        Response response = service.registerPlayer(gameid, name).execute();
+        if(response.code() == 200) {
+            System.out.println("register successful");
+        } else {
+            System.out.println("Error. Code: " + response.code());
+        }
     }
 }
--- restopoly-client/src/main/java/de/hawhamburg/vs/restopoly/api/GameService.java
+++ restopoly-client/src/main/java/de/hawhamburg/vs/restopoly/api/GameService.java
@@ -1,10 +1,10 @@
 package de.hawhamburg.vs.restopoly.api;
 
+import com.squareup.okhttp.ResponseBody;
 import de.hawhamburg.vs.restopoly.responses.GameCreateResponse;
 import de.hawhamburg.vs.restopoly.responses.GameDTO;
 import de.hawhamburg.vs.restopoly.responses.PlayerDTO;
 import retrofit.Call;
-import retrofit.Response;
 import retrofit.http.*;
 
 import java.util.List;
@@ -17,10 +17,10 @@ public interface GameService {
     Call<GameCreateResponse> createGame();
 
     @PUT("/games/{gameid}/players/{playerid}")
-    void registerPlayer(@Path("gameid") int gameId, @Path("playerid") String player);
+    Call<ResponseBody> registerPlayer(@Path("gameid") int gameId, @Path("playerid") String player);
 
     @PUT("/games/{gameid}/players/{playerid}/ready")
-    void setReady(@Path("gameid") int gameId, @Path("playerid") String player);
+    Call<ResponseBody> setReady(@Path("gameid") int gameId, @Path("playerid") String player);
 
     @GET("/games/{gameid}/players/{playerid}/ready")
     boolean checkPlayerReady(@Path("gameid") int gameId, @Path("playerid") String player);
@@ -32,10 +32,10 @@ public interface GameService {
     Call<PlayerDTO> getCurrentPlayer(@Path("gameid") int gameId);
 
     @PUT("/games/{gameid}/players/turn")
-    Response acquireMutex(@Path("gameid") int gameId, @Body PlayerDTO player);
+    Call<ResponseBody> acquireMutex(@Path("gameid") int gameId, @Body PlayerDTO player);
 
     @DELETE("/games/{gameid}/players/turn")
-    Response releaseMutex(@Path("gameid") int gameId, @Query("player") String player);
+    Call<ResponseBody> releaseMutex(@Path("gameid") int gameId, @Query("player") String player);
 
     @GET("/games/{gameid}/players/turn")
     Call<PlayerDTO> getPlayerHoldingMutex(@Path("gameid") int gameId);

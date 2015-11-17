42824d01c38d02d41fbb750b5a4e5ab353fea1de
Blackgen <jandb07@gmx.de>
2015-11-17 14:35:31 +0100

    Add Bank Entities

---
+++ src/main/java/de/hawhamburg/vs/restopoly/manager/BankManager.java
@@ -0,0 +1,30 @@
+package de.hawhamburg.vs.restopoly.manager;
+
+
+import de.hawhamburg.vs.restopoly.model.Bank;
+import de.hawhamburg.vs.restopoly.model.Game;
+import de.hawhamburg.vs.restopoly.model.Player;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class BankManager {
+    private Map<Game, Bank> banks = new HashMap<>();
+    private int bankCount;
+
+    public BankManager() {}
+
+    public Bank createBank(Game gameID) {
+        if (banks.containsKey(gameID)) throw new DuplicateKeyException("Bank already exists for game "+gameID);
+        Bank b = new Bank();
+        banks.put(gameID,b);
+        return b;
+    }
+
+    public Bank getBank(Game gameID) {
+        return banks.get(gameID);
+    }
+}
---
+++ src/main/java/de/hawhamburg/vs/restopoly/model/Bank.java
@@ -0,0 +1,52 @@
+package de.hawhamburg.vs.restopoly.model;
+
+import org.springframework.dao.DuplicateKeyException;
+
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+/**
+ * Created by JanDennis on 17.11.2015.
+ */
+public class Bank {
+    private Map<Player, BankAccount> accounts;
+
+    public Bank() {}
+
+    public void createAccount(Player playerID) {
+        if (accounts.containsKey(playerID)) throw new DuplicateKeyException("Account already exists for playerID "+playerID);
+        else {
+            accounts.put(playerID,new BankAccount(playerID));
+        }
+    }
+
+    public int getBalance(Player playerID) {
+        if (accounts.containsKey(playerID)) {
+            return accounts.get(playerID).getBalance();
+        } else {
+            throw new NoSuchElementException("No Account found for Player "+playerID);
+        }
+    }
+
+    public void addAmount(Player playerID, int amount) {
+        accounts.get(playerID).addBalance(amount);
+    }
+
+    public void subtractAmount(Player playerID, int amount) {
+        accounts.get(playerID).subtractAmount(amount);
+    }
+
+    public void transferAmount(Player fromPlayer, Player toPlayer, int amount) {
+        BankAccount fromAccount=accounts.get(fromPlayer);
+        BankAccount toAccount=accounts.get(toPlayer);
+
+        fromAccount.subtractAmount(amount);
+        toAccount.addBalance(amount);
+    }
+
+
+    public void createAccount(Player player, int amount) {
+        createAccount(player);
+        addAmount(player,amount);
+    }
+}

1de9c2d3808c264489e1f32862ff8dd38110ab1b
Tim Hagemann <tim@infinityblade.de>
2015-11-17 22:47:00 +0100

    Merge with upstream

--- src/main/java/de/hawhamburg/vs/restopoly/controllers/BoardController.java
+++ src/main/java/de/hawhamburg/vs/restopoly/controllers/BoardController.java
@@ -8,13 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
-
-import javax.websocket.server.PathParam;
 
 @RestController
 public class BoardController {
@@ -29,7 +24,7 @@ public class BoardController {
     private RestTemplate restTemplate = new RestTemplate();
 
     @RequestMapping(method = RequestMethod.PUT, value = "/boards/{gameid}/players/{playerid}")
-    public ResponseEntity placePlayer(@PathParam("gameid") int gameid, @PathParam("playerid") String playerid, @RequestBody Player player) {
+    public ResponseEntity placePlayer(@PathVariable("gameid") int gameid, @PathVariable("playerid") String playerid, @RequestBody Player player) {
         return this.gameBoardManager.getBoard(gameid).map(b -> {
             player.setId(playerid);
             b.addPlayer(player);
@@ -38,7 +33,7 @@ public class BoardController {
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/boards/{gameid}/players/{playerid}")
-    public ResponseEntity removePlayer(@PathParam("gameid") int gameid, @PathParam("playerid") String playerid) {
+    public ResponseEntity removePlayer(@PathVariable("gameid") int gameid, @PathVariable("playerid") String playerid) {
         return this.gameBoardManager.getBoard(gameid).map(b -> {
             b.removePlayer(playerid);
             return new ResponseEntity(HttpStatus.OK);
@@ -46,7 +41,7 @@ public class BoardController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "/boards/{gameid}/players/{playerid}/roll")
-    public ResponseEntity movePlayer(@PathParam("gameid") int gameid, @PathParam("playerid") String playerid, @RequestBody ThrowDTO rolls) {
+    public ResponseEntity movePlayer(@PathVariable("gameid") int gameid, @PathVariable("playerid") String playerid, @RequestBody ThrowDTO rolls) {
         String turnCheckUrl = this.mainServiceUrl + MUTEX_CHECK_URL.replace("{gameid}", gameid + "");
         return this.gameBoardManager.getBoard(gameid).filter(b -> b.getPositions().containsKey(playerid)).map(b -> {
             Player player = this.restTemplate.getForObject(turnCheckUrl, Player.class);
@@ -60,7 +55,7 @@ public class BoardController {
     }
 
     @RequestMapping("/boards/{gameid}/players/{playerid}")
-    public ResponseEntity<Player> getPlayerPosition(@PathParam("gameid") int gameid, @PathParam("playerid") String playerid) {
+    public ResponseEntity<Player> getPlayerPosition(@PathVariable("gameid") int gameid, @PathVariable("playerid") String playerid) {
         return this.gameBoardManager.getBoard(gameid).map(b -> b.getPositions().get(playerid)).map(pos -> {
             Player pl = new Player(playerid);
             pl.setPosition(pos);
@@ -69,9 +64,17 @@ public class BoardController {
     }
 
     @RequestMapping("/boards/{gameid}")
-    public ResponseEntity<GameBoard> getBoard(@PathParam("gameid") int gameid) {
+    public ResponseEntity<GameBoard> getBoard(@PathVariable("gameid") int gameid) {
         return this.gameBoardManager.getBoard(gameid).map(b -> new ResponseEntity<>(b, HttpStatus.OK)).orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-
+    @RequestMapping(method = RequestMethod.POST, value = "/boards/{gameid}")
+    public ResponseEntity createBoard(@PathVariable("gameid") int gameid) {
+        if(this.gameBoardManager.getBoard(gameid).isPresent()) {
+            return new ResponseEntity(HttpStatus.CONFLICT);
+        } else {
+            this.gameBoardManager.createBoard(gameid);
+            return new ResponseEntity(HttpStatus.CREATED);
+        }
+    }
 }

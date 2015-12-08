0688f843624c5c62140da344ab564fe0fbc70ace
Blackgen <jandb07@gmx.de>
2015-12-08 14:13:32 +0100

    Player has to pay rent now!

--- brokers/src/main/java/de/hawhamburg/vs/restopoly/controllers/BrokerController.java
+++ brokers/src/main/java/de/hawhamburg/vs/restopoly/controllers/BrokerController.java
@@ -1,6 +1,5 @@
 package de.hawhamburg.vs.restopoly.controllers;
 
-import de.hawhamburg.vs.restopoly.data.errors.AlreadyExistsException;
 import de.hawhamburg.vs.restopoly.data.model.Broker;
 import de.hawhamburg.vs.restopoly.data.model.Estate;
 import de.hawhamburg.vs.restopoly.data.model.Event;
@@ -12,9 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 /**
@@ -27,11 +24,12 @@ public class BrokerController {
     private BrokerManager brokerManager;
 
     private final String bankTransferUrl = "/banks/%d/transfer/from/%s/to/%s/%d";
+    private final String bankBuyUrl = "/banks/%d/transfer/from/%s/%d";
 
     // Create Broker
-    @RequestMapping(method = RequestMethod.PUT,value = "/brokers/{gameid}")
+    @RequestMapping(method = RequestMethod.PUT, value = "/brokers/{gameid}")
     public HttpStatus createBoard(@PathVariable("gameid") int gameid) {
-        if(this.brokerManager.getBroker(gameid).isPresent()) {
+        if (this.brokerManager.getBroker(gameid).isPresent()) {
             return HttpStatus.CONFLICT;
         } else {
             this.brokerManager.createBroker(gameid);
@@ -44,23 +42,31 @@ public class BrokerController {
     public ResponseEntity<String> putNewPlace(@PathVariable("gameid") int gameid, @PathVariable("placeid") String placeid,
                                               @RequestBody Estate newPlace) {
         Optional<Broker> broker = brokerManager.getBroker(gameid);
-        if (broker.isPresent()) (broker.get()).addPlace(placeid,newPlace);
-        return new ResponseEntity<String>(placeid,HttpStatus.OK);
+        if (broker.isPresent()) (broker.get()).addPlace(placeid, newPlace);
+        return new ResponseEntity<String>(placeid, HttpStatus.OK);
         //ToDO 201 Status bei neuerstellung
     }
 
     // Player vistited Place
     @RequestMapping(method = RequestMethod.POST, value = "/brokers/{gameid}/places/{placeid}/visit/{playerid}")
     public ResponseEntity<Collection<Event>> postPlayerVisitsPlace(@PathVariable("gameid") int gameid, @PathVariable("placeid") String placeid, @PathVariable("playerid") String player) {
-
+        Broker broker = brokerManager.getBroker(gameid).get();
+        String owner = broker.getOwner(placeid).getName();
+        if (!owner.equals(player)) {
+            int amount = broker.getRent(placeid);
+            RestTemplate restTemplate = new RestTemplate();
+            String url = String.format(bankTransferUrl, gameid, player, owner, amount);
+            restTemplate.postForLocation(bankBuyUrl, "Player " + player + " visited " + placeid + " and paid " + amount + " rent");
+        }
+        return new ResponseEntity<Collection<Event>>(HttpStatus.OK);
     }
 
     // Returns Owner
     @RequestMapping("/brokers/{gameid}/places/{placeid}/owner")
     public ResponseEntity<Player> getOwner(@PathVariable("gameid") int gameid, @PathVariable("placeid") String place) {
         Player pl = brokerManager.getBroker(gameid).get().getOwner(place);
-        if (pl==null) return new ResponseEntity<Player>(HttpStatus.NOT_FOUND);
-        return new ResponseEntity<Player>(pl,HttpStatus.OK);
+        if (pl == null) return new ResponseEntity<Player>(HttpStatus.NOT_FOUND);
+        return new ResponseEntity<Player>(pl, HttpStatus.OK);
     }
 
     //ToDo Add Events
@@ -72,9 +78,9 @@ public class BrokerController {
         if (!broker.hasPlace(place)) return new ResponseEntity<Collection<Event>>(HttpStatus.NOT_FOUND);
         if (!owner.equals(player)) {
             RestTemplate restTemplate = new RestTemplate();
-            String url = String.format(bankTransferUrl,gameid,player,owner,broker.getValue(place));
-            restTemplate.postForLocation(url,"Player "+player+" bought "+place);
-            broker.setOwner(place,player);
+            String url = String.format(bankTransferUrl, gameid, player, owner, broker.getValue(place));
+            restTemplate.postForLocation(url, "Player " + player + " bought " + place);
+            broker.setOwner(place, player);
             return new ResponseEntity<Collection<Event>>(HttpStatus.OK);
         }
         //ToDo Event ?
@@ -83,9 +89,9 @@ public class BrokerController {
 
     // Buy Place, fail if not for Sale (Already owned by someone)
     @RequestMapping(method = RequestMethod.POST, value = "/broker/{gameid}/places/{placeid}/owner")
-    public ResponseEntity<Event> buyPlace(@PathVariable("gameid") int gameid,@PathVariable("placeid") String place, @RequestBody Player player) {
+    public ResponseEntity<Event> buyPlace(@PathVariable("gameid") int gameid, @PathVariable("placeid") String place, @RequestBody Player player) {
         Broker broker = brokerManager.getBroker(gameid).get();
-        if (broker.getOwner(place)==null) {
+        if (broker.getOwner(place) == null) {
             broker.setOwner(place, player);
             return new ResponseEntity<Event>(HttpStatus.OK);
         }

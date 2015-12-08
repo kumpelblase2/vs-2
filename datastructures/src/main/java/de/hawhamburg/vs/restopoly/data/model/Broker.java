bf4cde615b0a662a5d7b0e835ab4305e369dea82
Blackgen <jandb07@gmx.de>
2015-12-08 14:05:25 +0100

    Add current rent method to broker

--- datastructures/src/main/java/de/hawhamburg/vs/restopoly/data/model/Broker.java
+++ datastructures/src/main/java/de/hawhamburg/vs/restopoly/data/model/Broker.java
@@ -1,9 +1,5 @@
 package de.hawhamburg.vs.restopoly.data.model;
 
-import de.hawhamburg.vs.restopoly.data.errors.AlreadyExistsException;
-
-
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -16,21 +12,32 @@ public class Broker {
     public Boolean hasPlace(String Place) {
         return places.containsKey(Place);
     }
+
     public void addPlace(String name, Estate place) {
-        places.putIfAbsent(name,place);
+        places.putIfAbsent(name, place);
     }
+
     public void setOwner(String place, Player owner) {
         if (owners.containsKey(place)) {
-            owners.replace(place,owner);
+            owners.replace(place, owner);
         } else {
-            owners.put(place,owner);
+            owners.put(place, owner);
         }
     }
+
     public String getOwnerID(String place) {
         return places.get(place).getOwner();
     }
+
     public Player getOwner(String place) {
         return owners.get(place);
     }
-    public int getValue(String place) { return places.get(place).getValue();}
+
+    public int getValue(String place) {
+        return places.get(place).getValue();
+    }
+
+    public int getRent(String place) {
+        return places.get(place).getRent().get(places.get(place).getHouses());
+    }
 }

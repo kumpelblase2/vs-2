cb2e505b294625d3fb93acb9585d8621655f36cf
Tim Hagemann <tim@infinityblade.de>
2016-01-12 16:33:57 +0100

    Set Location Header in bank-service.

--- bank/src/main/java/de/hawhamburg/vs/restopoly/controllers/BankController.java
+++ bank/src/main/java/de/hawhamburg/vs/restopoly/controllers/BankController.java
@@ -7,19 +7,27 @@ import de.hawhamburg.vs.restopoly.data.model.*;
 import de.hawhamburg.vs.restopoly.data.dto.PlayerAndAmountDTO;
 import de.hawhamburg.vs.restopoly.manager.BankManager;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class BankController {
+    private static final String ACCOUNT_URL = "/banks/{gameid}/players/{playerid}";
+    private static final String BANKS_URL = "/banks/{gameid}";
+
     @Autowired
     private BankManager manager;
     private final RestTemplate restTemplate = new RestTemplate();
 
     @RequestMapping(method = RequestMethod.POST, value = "/banks/{gameid}/players")
-    public ResponseEntity createNewBankAccount(@PathVariable("gameid") int gameid, @RequestBody PlayerAndAmountDTO body) {
+    public ResponseEntity createNewBankAccount(@PathVariable("gameid") int gameid, @RequestBody PlayerAndAmountDTO body,
+                                               UriComponentsBuilder uriBuilder) {
         String player = body.getPlayer().getId();
         int amount = body.getAmount();
         Bank b = manager.getBank(gameid).orElseThrow(NotFoundException::new);
@@ -29,10 +37,13 @@ public class BankController {
             e.printStackTrace();
             return new ResponseEntity(HttpStatus.CONFLICT);
         }
-        return new ResponseEntity(HttpStatus.CREATED);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Location", uriBuilder.path(ACCOUNT_URL).buildAndExpand(gameid, player).toUriString());
+        return new ResponseEntity(headers, HttpStatus.CREATED);
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/banks/{gameid}/players/{playerid}")
+    @RequestMapping(method = RequestMethod.GET, value = ACCOUNT_URL)
     public Integer getAmount(@PathVariable("gameid") int gameid, @PathVariable("playerid") String playerid) {
         return manager.getBank(gameid).orElseThrow(NotFoundException::new).getBalance(playerid);
     }
@@ -64,9 +75,11 @@ public class BankController {
         return ev;
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/banks/{gameid}")
-    public void createNewBank(@PathVariable("gameid") int gameId, GameCreateDTO inCreateDTO) {
+    @RequestMapping(method = RequestMethod.PUT, value = BANKS_URL)
+    public void createNewBank(@PathVariable("gameid") int gameId, GameCreateDTO inCreateDTO, UriComponentsBuilder uriBuilder,
+                              HttpServletResponse response) {
         manager.createBank(gameId, inCreateDTO.getComponents());
+        response.setHeader("Location", uriBuilder.path(BANKS_URL).buildAndExpand(gameId).toUriString());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/banks/{gameid}/transfers/{transfer}")
@@ -80,7 +93,11 @@ public class BankController {
     }
 
     private void createTransfer(Components components, int gameid, Event ev) {
-        String result = this.restTemplate.postForObject(components.getEvents() + "/events?gameid=" + gameid, ev, String.class);
-        ev.setUri(result);
+        try {
+            String result = this.restTemplate.postForObject(components.getEvents() + "/events?gameid=" + gameid, ev, String.class);
+            ev.setUri(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

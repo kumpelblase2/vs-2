07930fc2013f3772c1d04034320931353ecfdbc8
Blackgen <jandb07@gmx.de>
2016-01-19 12:38:57 +0100

    Fix Brokermanager

--- brokers/src/main/java/de/hawhamburg/vs/restopoly/manager/BrokerManager.java
+++ brokers/src/main/java/de/hawhamburg/vs/restopoly/manager/BrokerManager.java
@@ -3,23 +3,27 @@ package de.hawhamburg.vs.restopoly.manager;
 import de.hawhamburg.vs.restopoly.data.model.Broker;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by JanDennis on 04.12.2015.
  */
 @Component
 public class BrokerManager {
-    private Map<Integer, Broker> brokers = new HashMap<>();
+    private Set<Broker> brokers = new HashSet<>();
+    private AtomicInteger brokerCount = new AtomicInteger();
+//    private Map<Integer, Broker> brokers = new HashMap<>();
 
 
-    public Broker createBroker(int gameid) {
-        return this.brokers.putIfAbsent(gameid,new Broker());
+    public Broker createBroker() {
+        int id = this.brokerCount.incrementAndGet();
+        Broker b = new Broker(id);
+        this.brokers.add(b);
+        return b;
     }
 
-    public Optional<Broker> getBroker(int gameId) {
-        return Optional.ofNullable(this.brokers.get(gameId));
+    public Optional<Broker> getBroker(int brokerId) {
+        return this.brokers.parallelStream().filter(bro -> bro.getId() == brokerId).findFirst();
     }
 }
